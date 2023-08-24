5fab0958eb87ed01e8813324cc5fd03b355efc2e
CarlosR8 <carloz88813@hotmail.com>
2023-08-24 03:03:26 -0500

    cpu user info

--- cpu.py
+++ cpu.py
@@ -7,6 +7,8 @@ import time
 import psutil
 from paho.mqtt import client as mqtt_client
 from sendDataBase import sendDataFireBase
+import platform
+import cpuinfo
 
 
 # Local
@@ -85,6 +87,52 @@ context = ssl.create_default_context()
 
 #   if cpu > 40 send mail
 TOPIC = "grupo6_arquitectura"
+
+#   Get Windows version
+
+windows = platform.system()
+release = platform.release()
+sistema = windows + ' ' + release
+
+#   Get processor info
+
+cpu = cpuinfo.get_cpu_info()
+cpu_info = str(cpu['brand_raw'])
+
+#   Get ram info
+
+memoria_ram = psutil.virtual_memory()
+ram = str(round(memoria_ram.total / (1024 ** 3),1)) + ' GB'
+
+#   Get disk info
+
+disco = psutil.disk_usage('/')
+disco_espacio = str(round(disco.total / (1024 **3), 1)) + ' GB'
+
+#   Get system_info
+
+arquitectura = platform.architecture()
+if arquitectura[0] == '64bit':
+    sistema_arq = "Sistema operativo de 64 bits, Procesador x64"
+else:
+    sistema_arq = "Sistema operativo de 32 bits, Procesador x86"
+
+'''
+data = {
+    'Sistema':  sistema,
+    'Cpu_info': cpu_info,
+    'Ram_info': ram,
+    'Disco_espacio':    disco_espacio,
+    'Tipo_sistema': sistema_arq
+}
+
+
+json_data = json.dumps(data)
+result= client.publish(TOPIC, json_data)
+print("Datos del sistema enviados")
+time.sleep(2)
+'''
+
 while True:
     cpu = psutil.cpu_percent()*10
     ram = psutil.virtual_memory()[2]
@@ -93,7 +141,12 @@ while True:
         data = {
             'CPU': cpu,
             'Memoria': ram,
-            'Disco': usoDisco
+            'Disco': usoDisco,
+            'Sistema':  sistema,
+            'Cpu_info': cpu_info,
+            'Ram_info': ram,
+            'Disco_espacio': disco_espacio,
+            'Tipo_sistema': sistema_arq
         }
         sendDataFireBase(cpu, ram, usoDisco)
 
